8c9000870a25cc6519d4128bb41cecbc4e98c513
Bhavana200430 <bhavanabommanalli@gmail.com>
2026-06-05 09:16:17 +0530

    Day 1 project setup

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define WIDTH 40
+#define HEIGHT 20
+#define EMPTY ' '
+#define PIXEL '*'
+
+char picture[HEIGHT][WIDTH];
+
+int main() {
+    printf("2D Graphics Editor Started!\n");
+    return 0;
+}
