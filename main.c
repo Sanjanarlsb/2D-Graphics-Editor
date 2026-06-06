78859279631018231e92b945edb9c364e79d6252
Bhavana200430 <bhavanabommanalli@gmail.com>
2026-06-06 09:36:33 +0530

    Day 2 added canvas initialization and display

--- main.c
+++ main.c
@@ -1,14 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define WIDTH 40
 #define HEIGHT 20
-#define EMPTY ' '
-#define PIXEL '*'
 
 char picture[HEIGHT][WIDTH];
 
-int main() {
-    printf("2D Graphics Editor Started!\n");
+// Fill the canvas with '_'
+void initializePicture()
+{
+    int i, j;
+
+    for(i = 0; i < HEIGHT; i++)
+    {
+        for(j = 0; j < WIDTH; j++)
+        {
+            picture[i][j] = '_';
+        }
+    }
+}
+
+// Display the canvas
+void displayPicture()
+{
+    int i, j;
+
+    for(i = 0; i < HEIGHT; i++)
+    {
+        for(j = 0; j < WIDTH; j++)
+        {
+            printf("%c ", picture[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    printf("2D Graphics Editor Started!\n\n");
+
+    initializePicture();
+    displayPicture();
+
     return 0;
 }
